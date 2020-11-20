44f43978a0fb494ee1b0bc4667e3f0e5772deddd
Max <starreveld@chapman.edu>
2020-11-20 11:11:56 -0800

    Fixed numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -3,11 +3,14 @@
 # Max Starreveld
 echo "Enter a positive number:"
 read NUMBER
-for (( i = 1; i <= $NUMBER; i++ )); do
-  if [ $((i % 2)) -eq 0 ]
-  then
-    echo "$i even"
-  else
-    echo "$i odd"
-  fi
+COUNTER=1
+while [ $COUNTER -le "$NUMBER" ]
+do
+	if [ $((COUNTER % 2)) -eq 0 ]
+	then
+		echo "$COUNTER is even"
+	else
+		echo "$COUNTER is odd"
+	fi
+	COUNTER=$((COUNTER + 1))
 done
